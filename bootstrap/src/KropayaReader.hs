751cf9fd14d1f5c7eef0dbf3e98d4d56d3cfd154
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2014-12-24 21:37:31 +1300

    Avoid string literals; use separate char lits instead.

--- bootstrap/src/KropayaReader.hs
+++ bootstrap/src/KropayaReader.hs
@@ -13,23 +13,28 @@ import KropayaTypes
 top :: Atomic = number !.
 
 number :: Atomic
-  = hinteger
+  = kdecimal
+  / hinteger
   / dinteger
 
 sign :: Maybe Char
   = [-+]?
 
+kdecimal :: Atomic
+  = sign [0-9]+ "." [0-9]+ { KDec (sigrat $1 ($2 ++ "." ++ $3)) }
+
 dinteger :: Atomic
   = sign [0-9]+ { KInt (sigdec $1 $2) }
 
 hinteger :: Atomic
-  = sign "0x" [0-9a-fA-F]+ { KInt (sighex $1 $2) }
+  = sign '0' 'x' [0-9a-fA-F]+ { KInt (sighex $1 $2) }
 |]
 
 sighex = sigthing hexadecimal
 sigdec = sigthing decimal
+sigrat = sigthing rational
 
-sigthing :: Reader Integer -> Maybe Char -> String -> Integer
+sigthing :: Num n => Reader n -> Maybe Char -> String -> n
 sigthing thing (Just sign) mag = case signed thing (pack (sign : mag)) of Right (number, _) -> number
                                                                           _                 -> 0 
 sigthing thing _ mag = case thing (pack mag) of Right (number, _) -> number
