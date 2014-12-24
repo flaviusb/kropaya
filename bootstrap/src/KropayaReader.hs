bb2d680adcdc05b1b9b904d66949fb06ee6a3629
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2014-12-24 21:04:13 +1300

    Start pulling reader code into reader module.

---
+++ bootstrap/src/KropayaReader.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TemplateHaskell, QuasiQuotes, FlexibleContexts #-}
+module KropayaReader(top, number) where
+
+import Text.Peggy
+import Numeric
+import Data.Text
+import Data.Text.Read
+import Data.Maybe
+
+import KropayaTypes
+
+[peggy|
+top :: Integer = expr !.
+
+expr :: Integer
+  = expr "+" expr { $1 + $2 }
+  / expr "-" expr { $1 - $2 }
+  / number
+
+number :: Integer
+  = hinteger
+  / dinteger
+
+sign :: Maybe Char
+  = [-+]?
+
+dinteger :: Integer
+  = sign [0-9]+ { sigdec $1 $2 }
+
+hinteger :: Integer
+  = sign "0x" [0-9a-fA-F]+ { sighex $1 $2 }
+|]
+
+sighex = sigthing hexadecimal
+sigdec = sigthing decimal
+
+sigthing :: Reader Integer -> Maybe Char -> String -> Integer
+sigthing thing (Just sign) mag = case signed thing (pack (sign : mag)) of Right (number, _) -> number
+                                                                          _                 -> 0 
+sigthing thing _ mag = case thing (pack mag) of Right (number, _) -> number
+                                                _                 -> 0 
+
--- bootstrap/src/Main.hs
+++ bootstrap/src/Main.hs
@@ -8,38 +8,9 @@ import Data.Text.Read
 import Data.Maybe
 
 import KropayaTypes
+import KropayaReader
 
-[peggy|
-top :: Integer = expr !.
-
-expr :: Integer
-  = expr "+" expr { $1 + $2 }
-  / expr "-" expr { $1 - $2 }
-  / number
-
-number :: Integer
-  = hinteger
-  / dinteger
-
-sign :: Maybe Char
-  = [-+]?
-
-dinteger :: Integer
-  = sign [0-9]+ { sigdec $1 $2 }
-
-hinteger :: Integer
-  = sign "0x" [0-9a-fA-F]+ { sighex $1 $2 }
-|]
-
-sighex = sigthing hexadecimal
-sigdec = sigthing decimal
-
-sigthing :: Reader Integer -> Maybe Char -> String -> Integer
-sigthing thing (Just sign) mag = case signed thing (pack (sign : mag)) of Right (number, _) -> number
-                                                                          _                 -> 0 
-sigthing thing _ mag = case thing (pack mag) of Right (number, _) -> number
-                                                _                 -> 0 
 
 main :: IO ()
 main =
-  print . parseString dinteger "" =<< getContents
+  print . parseString number "" =<< getContents
