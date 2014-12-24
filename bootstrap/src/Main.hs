8106cbf24528d5c3d461f882e18dc16ab3ec5d04
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2014-12-24 14:41:48 +1300

    dinteger and hinteger

--- bootstrap/src/Main.hs
+++ bootstrap/src/Main.hs
@@ -1,3 +1,43 @@
+{-# LANGUAGE TemplateHaskell, QuasiQuotes, FlexibleContexts #-}
 module Main where
 
-main = putStrLn ""
+import Text.Peggy
+import Numeric
+import Data.Text
+import Data.Text.Read
+import Data.Maybe
+
+[peggy|
+top :: Integer = expr !.
+
+expr :: Integer
+  = expr "+" expr { $1 + $2 }
+  / expr "-" expr { $1 - $2 }
+  / number
+
+number :: Integer
+  = hinteger
+  / dinteger
+
+sign :: Maybe Char
+  = [-+]?
+
+dinteger :: Integer
+  = sign [0-9]+ { sigdec $1 $2 }
+
+hinteger :: Integer
+  = sign "0x" [0-9a-fA-F]+ { sighex $1 $2 }
+|]
+
+sighex = sigthing hexadecimal
+sigdec = sigthing decimal
+
+sigthing :: Reader Integer -> Maybe Char -> String -> Integer
+sigthing thing (Just sign) mag = case signed thing (pack (sign : mag)) of Right (number, _) -> number
+                                                                          _                 -> 0 
+sigthing thing _ mag = case thing (pack mag) of Right (number, _) -> number
+                                                _                 -> 0 
+
+main :: IO ()
+main =
+  print . parseString dinteger "" =<< getContents
