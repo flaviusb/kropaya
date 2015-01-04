d9132182b3d3e398aecd27225cfba1fa05b91e9e
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2015-01-04 23:10:39 +1300

    Move around some types.

--- bootstrap/src/KropayaTypes.hs
+++ bootstrap/src/KropayaTypes.hs
@@ -1,32 +1,40 @@
-module KropayaTypes(Atomic(..), Row(..), Label(..), Predicate(..), Lambda(..), Arglist(..), Code(..), R0Type(..)) where
+module KropayaTypes(Atomic(..), Row(..), Label(..), Predicate(..), RowOp(..), TRow(..), Lambda(..), Arglist(..), Code(..), R0Type(..)) where
 
 import Numeric
 import Data.Text
 import Data.Text.Read
 import Data.Maybe
 
-data Atomic   = KInt     Integer
-              | KDec     Double
-              | KSSt     Text
-              | KQSt     [Either [Code] Text]
-              | KSym     Text     deriving (Show, Eq)
+data Atomic    = KInt     Integer
+               | KDec     Double
+               | KSSt     Text
+               | KQSt     [Either [Code] Text] -- This is an interpolated string, made of Text and Code segments
+               | KSym     Text     deriving (Show, Eq)
 
-data Row      = Row [Label] [Row] deriving (Show, Eq)
-data Label    = Pinned Text R0Type
-              | Polymorphic Text R0Type
-              | Literal Text R0Type deriving (Show, Eq)
+data Row       = Row [Label] deriving (Show, Eq)
+data Label     = Pinned Text R0Type
+               | Polymorphic Text R0Type
+               | Literal Text R0Type deriving (Show, Eq)
 
-data Predicate = Lacks Label
-               | Equals Row
+data Predicate = Lacks Row Label
+               | Equals Row Row deriving (Show, Eq)
 
-data TRow     = Product Row
-              | Sum Row deriving (Show, Eq)
+data RowOp     = Extend   Row Label
+               | Restrict Row Label deriving (Show, Eq)
 
-data Lambda   = Lambda Arglist Code deriving (Show, Eq)
-data Arglist  = Arglist [Text] deriving (Show, Eq)
-data Code     = KR0Type R0Type
-              | NL             deriving (Show, Eq)
+data TRow      = Product Row
+               | Sum Row deriving (Show, Eq)
 
-data R0Type   = KAtomic  Atomic
-              | KRow     TRow
-              | KLambda  Lambda  deriving (Show, Eq)
+data Lambda    = Lambda Arglist Code deriving (Show, Eq)
+data Arglist   = Arglist [Text] deriving (Show, Eq)
+data Code      = KR0Type R0Type
+               | NL             deriving (Show, Eq)
+
+-- R0Types are non-dependent principle types; dependent principle types will be indexed to avoid girards paradox a la ramified types
+-- This is a giant TODO, really :-(
+data R0Type   = KAtomic    Atomic
+              | KRow       TRow
+              | KLabel     Label
+              | KPredicate Predicate
+              | KRowOp     RowOp
+              | KLambda    Lambda  deriving (Show, Eq)
