62b34b6684cb3ea707b71e91f2d1fd334102974a
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2015-05-24 22:44:32 +1200

    Start adding some builder functions.

--- bootstrap/bootstrap-kropaya-interpreter.el
+++ bootstrap/bootstrap-kropaya-interpreter.el
@@ -14,11 +14,25 @@
 (defun make-primitive (tag val)
     (list tag val))
 
-(defun make-sum (&rest pairs)
-  (make-primitive 'sum pairs))
+;; Sum and product pairs are an alist of labe
+(defun make-type-row (kind pairs)
+  (make-primitive kind pairs))
 
-(defun make-product (&rest pairs)
+(defun make-sum (pair)
+  (make-primitive 'sum pair))
+
+(defun make-product (pairs)
   (make-primitive 'product pairs))
+
+(defun make-module (name quantifiers forest)
+  (make-primitive 'module `((name . ,name) (quantifiers . ,quantifiers) (forest . ,forest))))
+
+;(setq prelude (make-module 'prelude '() '()))
+;
+;(print prelude)
+;(print (cadr prelude))
+;
+;(print (assq 'name (cadr prelude)))
 
 ;; AST
 
